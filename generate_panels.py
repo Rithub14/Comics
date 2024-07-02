b908d6815b5a8b4ba53fd3377178166ffdf60dac
Rizwan Aslam <letsmailriz@gmail.com>
2024-07-02 07:07:22 +0200

    Updated codebase

--- generate_panels.py
+++ generate_panels.py
@@ -1,7 +1,7 @@
 
 import re
 
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
